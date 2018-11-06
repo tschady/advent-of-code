47ea39332a31e54e2f964690b55836eb9fe39851
Tom Schady <tschady@gmail.com>
2018-11-05 19:34:25 -0600

    Solve day 1

---
+++ src/aoc_2017/d01.clj
@@ -0,0 +1,38 @@
+(ns aoc-2017.d01
+  (:require [aoc-2017.util :as util]))
+
+(def input (util/read-file "d01.txt"))
+
+(defn- solve-captcha
+  "Returns the captcha 'checksum' of a given collection of paired 2-tuple
+  numbers represented as characters.  Result is the sum of the values of
+  each pairing that are equivalent."
+  [pairs]
+  (->> pairs
+       (filter (partial apply =))
+       (map first)
+       (map #(Character/getNumericValue %))
+       (reduce + 0)))
+
+(defn part-1
+  "Solve for captcha where pairs are adjacent, with the last and first char paired
+  from the given input string `s`."
+  [s]
+  (let [pairings (partition 2 1 [(first s)] s)]
+    (solve-captcha pairings)))
+
+(defn part-2
+  "Solve for captcha where pairs are 'halfway' around the circle of numbers
+  from the given input string `s`, which must be of even length."
+  [s]
+  {:pre [(zero? (rem (count s) 2))]} ; problem asserts even length input
+  (let [mid-point (/ (count s) 2)
+        pairings (->> (seq s)
+                      (split-at mid-point)
+                      (apply map list))]
+    ;; the circular list pairings are symmetrical, just multiply by 2
+    (* 2 (solve-captcha pairings))))
+
+(defn solve []
+  [(part-1 input)
+   (part-2 input)])
---
+++ src/aoc_2017/util.clj
@@ -0,0 +1,24 @@
+(ns aoc-2017.util
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(defn read-file
+  "Return full file contents from `path`."
+  [path]
+  (-> path io/resource slurp str/trim-newline))
+
+(defn read-file-rows
+  "Return file contents as collection of rows."
+  [path]
+  (-> path read-file str/split-lines))
+
+(defn read-tsv
+  "Return a list of rows, with each row as a list of values from the TSV."
+  [path]
+  (->> path read-file-rows (map #(str/split %1 #"\t"))))
+
+(defn explode-digits
+  "Turn input string of digits into sequence of numbers they represent."
+  [s]
+  (map #(Character/getNumericValue %) s))
+
