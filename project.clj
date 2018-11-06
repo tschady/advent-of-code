f92e2c0a72cef81c01c7b03f0e1e8f41f7907e05
Tom Schady <tschady@gmail.com>
2018-11-05 19:33:56 -0600

    Add modified leiningen boilerplate

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject aoc-2017 "0.1.0-SNAPSHOT"
+  :description "Clojure solutions for Advent of Code 2017"
+  :url "http://adventofcode.com"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :main ^:skip-aot aoc-2017.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
---
+++ test/aoc_2017/core_test.clj
@@ -0,0 +1,3 @@
+(ns aoc-2017.core-test
+  (:require [clojure.test :refer :all]
+            [aoc-2017.core :refer :all]))
