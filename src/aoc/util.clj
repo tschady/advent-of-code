071fd88b200b3cd98282ea2bc30bada85cc79bf8
tschady <tschady@users.noreply.github.com>
2018-11-11 07:52:04 -0600

    Change fn name for clarity

--- src/aoc/util.clj
+++ src/aoc/util.clj
@@ -7,7 +7,7 @@
   [path]
   (-> path io/resource slurp str/trim-newline))
 
-(defn read-file-rows
+(defn read-lines
   "Return file contents as collection of rows."
   [path]
   (-> path read-file str/split-lines))
@@ -15,7 +15,7 @@
 (defn read-tsv
   "Return a list of rows, with each row as a list of values from the TSV."
   [path]
-  (->> path read-file-rows (map #(str/split %1 #"\t"))))
+  (->> path read-lines (map #(str/split %1 #"\t"))))
 
 (defn explode-digits
   "Turn input string of digits into sequence of numbers they represent."
