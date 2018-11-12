73032a49f6f09325350b86f55bea358f30c51ff1
tschady <tschady@users.noreply.github.com>
2018-11-12 07:42:07 -0600

    Solve day 5

---
+++ src/aoc/2017/d05.clj
@@ -0,0 +1,24 @@
+(ns aoc.2017.d05
+  (:require [aoc.util :as util]))
+
+(def input (->> "2017/d05.txt" util/read-lines (mapv #(Integer/parseInt %))))
+
+(defn steps-to-exit
+  [tape update-fn]
+  (loop [loc 0
+         tape tape
+         steps 1]
+    (let [cur-val (get tape loc)
+          new-loc (+ loc cur-val)
+          new-tape (update tape loc update-fn)]
+      (if (>= new-loc (count tape))
+        steps
+        (recur new-loc new-tape (inc steps))))))
+
+(defn p2-offset-fn [n] (if (> n 2) (dec n) (inc n)))
+
+(defn part-1 [input]
+  (steps-to-exit input inc))
+
+(defn part-2 [input]
+  (steps-to-exit input p2-offset-fn))
