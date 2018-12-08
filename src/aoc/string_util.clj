86fc28a34741df0134e7bbf0c5d455825496b238
Tom Schady <tschady@gmail.com>
2018-12-08 09:13:57 -0600

    Add string difference utilities

--- src/aoc/string_util.clj
+++ src/aoc/string_util.clj
@@ -12,3 +12,17 @@
 (defn anagram? [word candidate]
   (and (not= (str/lower-case word) (str/lower-case candidate))
        (= (alphagram word) (alphagram candidate))))
+
+(defn hamming-distance
+  "The Hamming distance between two strings of equal length is the
+  number of positions at which the corresponding symbols are different.
+  Undefined (returns `nil`) for strings of unequal length."
+  [a b]
+  (when (= (count a) (count b))
+    (count (filter false? (map = a b)))))
+
+(defn string-intersection
+  "Return the common letters between two input strings, where common
+  letters must occur in the same corresponding position."
+  [a b]
+  (apply str (map #(when (= %1 %2) %1) a b)))
