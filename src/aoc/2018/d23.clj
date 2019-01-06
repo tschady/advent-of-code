abace45c45d5e7379ba19e84687390e78adc023f
tschady <tschady@users.noreply.github.com>
2019-01-06 08:02:21 -0600

    Solve d23 part 1 (#3)

---
+++ src/aoc/2018/d23.clj
@@ -0,0 +1,32 @@
+(ns aoc.2018.d23
+  (:require [aoc.file-util :as file-util]
+            [aoc.math-util :as math-util]))
+
+(def input (file-util/read-lines "2018/d23.txt"))
+
+(defn- parse-bot
+  "Given an input string of nanobot parameters, parse into hashmap."
+  [bot-str]
+  (let [[x y z r] (map read-string (re-seq #"-?\d+" bot-str))]
+    {:loc [x y z] :r r}))
+
+(defn- within-range?
+  "Predicate function returning whether the candidate bot is within
+  the subject bot's range, inclusively."
+  [subject candidate]
+  (>= (:r subject)
+      (math-util/manhattan-dist (:loc candidate) (:loc subject))))
+
+(defn part-1
+  "Calculate the number of bots within manhattan-distance of the nanobot
+  with the largest range."
+  [input]
+  (let [bots (map parse-bot input)
+        big-bot (apply max-key :r bots)]
+    (count (filter (partial within-range? big-bot) bots))))
+
+(defn part-2
+  "Return the manhattan distance from the origin to the closest coordinates
+  that are in range of the largest number of nanobots."
+  [input]
+  true)
---
+++ src/aoc/math_util.clj
@@ -0,0 +1,7 @@
+(ns aoc.math-util)
+
+(defn manhattan-dist
+  "Return the manhattan distance between two points, where each point is a
+  vector of cartesian coordinates.  Works for n-dimensions."
+  [a b]
+  (reduce + (map (comp #(Math/abs ^long %) -) a b)))
