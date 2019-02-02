d71d0c1e53d3a82a60535a5e6af30e6464323c28
Tom Schady <tschady@gmail.com>
2019-02-02 11:38:35 -0600

    Solve d1p2, refactor first solution

--- src/aoc/2016/d01.clj
+++ src/aoc/2016/d01.clj
@@ -8,7 +8,7 @@
 (def bearings [[0 -1] [1 0] [0 1] [-1 0]])
 (def dir->adjust-bearing {\R inc, \L dec})
 
-(def origin {:dir 0, :loc [0 0]})
+(def origin [0 0])
 
 (defn parse-moves
   "Transform CSV moves into vector of moves, with each move as a tuple:
@@ -18,25 +18,32 @@
   (->> (str/split move-str #", ")
        (map (juxt first #(Integer/parseInt (apply str (rest %)))))))
 
-(defn turn
-  [pos dir]
-  (update pos :dir (comp #(mod % 4) (dir->adjust-bearing dir))))
-
-(defn move
-  [pos steps]
-  (let [dist (mapv (partial * steps) (bearings (:dir pos)))]
-    (update pos :loc (partial mapv + dist))))
+(defn steps
+  "Determine series of cartesian deltas from list of moves."
+  [moves]
+  (let [turns (map first moves)
+        dists (map second moves)
+        indexes (->> turns
+                     (reductions #(mod ((dir->adjust-bearing %2) %1) 4) 0)
+                     rest)
+        deltas (map bearings indexes)]
+    (mapcat #(repeat %1 %2) dists deltas)))
 
 (defn walk
-  [start moves]
-  (reduce (fn [pos [dir steps]] (-> pos
-                                    (turn dir)
-                                    (move steps)))
-          start
-          moves))
+  "Return every location reached by applying steps given by deltas."
+  [deltas from]
+  (reductions (partial mapv +) from deltas))
 
 (defn part-1
-  ""
+  "Return Manhattan distance from origin determined by walking input."
   [input]
-  (let [dest (walk origin (parse-moves input))]
-    (math-util/manhattan-dist (:loc origin) (:loc dest))))
+  (let [dest (-> input parse-moves steps (walk origin) last)]
+    (math-util/manhattan-dist origin dest)))
+
+(defn part-2
+  "Return Manhattan distance from origin of first location reached twice
+  by walking given input."
+  [input]
+  (let [locs (-> input parse-moves steps (walk origin))
+        dup (first (math-util/first-duplicate locs))]
+    (math-util/manhattan-dist origin dup)))
