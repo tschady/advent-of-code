c202a2d6097a13794372a4d05a45654438f78433
Tom Schady <tschady@gmail.com>
2019-02-02 11:38:35 -0600

    Solve d1p2, refactor first solution

--- src/aoc/math_util.clj
+++ src/aoc/math_util.clj
@@ -5,3 +5,15 @@
   vector of cartesian coordinates.  Works for n-dimensions."
   [a b]
   (reduce + (map (comp #(Math/abs ^long %) -) a b)))
+
+(defn first-duplicate
+  "For given coll, return a vector of first duplicate entry, and the two
+  indexes of each occurence.
+  e.g. [<target entry> [<first index> <matching index>]"
+  [coll]
+  (reduce (fn [acc [idx x]]
+            (if-let [v (get acc x)]
+              (reduced [x (conj v idx)])
+              (assoc acc x [idx])))
+          {} (map-indexed #(vector % %2) coll)))
+
