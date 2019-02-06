f05cd5c15d58da776ac065839fc027707a179d8d
tschady <tschady@users.noreply.github.com>
2019-02-06 04:46:18 -0600

    Solve 2015 d4

---
+++ src/aoc/2015/d04.clj
@@ -0,0 +1,19 @@
+(ns aoc.2015.d04
+  (:require [pandect.algo.md5 :refer [md5]]
+            [clojure.string :as str]))
+
+(def input "iwrupvqb")
+
+(defn- solve
+  "Return first positive integer that when added after 'secret',
+  the resulting md5 hash begins with the target string."
+  [secret target]
+  (first (for [n (range)
+               :let [candidate (str secret n)
+                     digest (md5 candidate)]
+               :when (str/starts-with? digest target)]
+           n)))
+
+(defn part-1 [secret] (solve secret "00000"))
+
+(defn part-2 [secret] (solve secret "000000"))
