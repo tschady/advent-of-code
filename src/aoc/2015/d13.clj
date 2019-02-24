f7d844979bc253cb60e16059e90a9775bfd9fcf9
tschady <tschady@users.noreply.github.com>
2019-02-24 13:03:06 -0600

    Solve 2015 d13

---
+++ src/aoc/2015/d13.clj
@@ -0,0 +1,54 @@
+(ns aoc.2015.d13
+  (:require [aoc.file-util :as file-util]
+            [clojure.set :as set]
+            [clojure.math.combinatorics :as combo]))
+
+(def input (file-util/read-lines "2015/d13.txt"))
+
+(def data-pattern #"^(\w+) would (gain|lose) (\d+) happiness units by sitting next to (\w+).$")
+
+(defn- parse-input-line [s]
+  (let [[_ a sign-str amt b] (re-find data-pattern s)
+        sign (if (= "lose" sign-str) -1 1)]
+    {#{a b} (* sign (Integer/parseInt amt))}))
+
+(defn- seat-scores
+  "Return map of pairings to score.  Pairings denote table neighbors
+  e.g. #{Alice Bob} 127"
+  [coll]
+  (apply merge-with + (map parse-input-line coll)))
+
+(defn- diners
+  "Return list of all diners in the guest list."
+  [graph]
+  (->> graph keys (apply set/union) vec))
+
+(defn- pairings
+  "Given ordered circular sequence of guests, return all neighbor pairings."
+  [xs]
+  (map set (partition 2 1 (vector (first xs)) xs)))
+
+(defn- solve
+  "Return max happiness score based on optimal configuration of `guests`
+  given happiness score pairings given by `scores`."
+  [guests scores]
+  (->> (combo/permutations guests)
+       (map pairings)
+       (map (partial keep scores))
+       (map (partial reduce + 0))
+       (apply max)))
+
+(defn part-1
+  "Return max happiness score for given guest list and scores."
+  [coll]
+  (let [scores (seat-scores coll)
+        guests (diners scores)]
+    (solve guests scores)))
+
+(defn part-2
+  "Return max happiness score for given guest list and scores,
+  including 'myself' as a guest of neutral pairwise happiness."
+  [coll]
+  (let [scores (seat-scores coll)
+        guests (conj (diners scores) :me)]
+    (solve guests scores)))
---
+++ test/aoc/2015/d13_test.clj
@@ -0,0 +1,24 @@
+(ns aoc.2015.d13-test
+  (:require [aoc.2015.d13 :as sut]
+            [clojure.test :refer :all]))
+
+(def test-input
+  ["Alice would gain 54 happiness units by sitting next to Bob."
+   "Alice would lose 79 happiness units by sitting next to Carol."
+   "Alice would lose 2 happiness units by sitting next to David."
+   "Bob would gain 83 happiness units by sitting next to Alice."
+   "Bob would lose 7 happiness units by sitting next to Carol."
+   "Bob would lose 63 happiness units by sitting next to David."
+   "Carol would lose 62 happiness units by sitting next to Alice."
+   "Carol would gain 60 happiness units by sitting next to Bob."
+   "Carol would gain 55 happiness units by sitting next to David."
+   "David would gain 46 happiness units by sitting next to Alice."
+   "David would lose 7 happiness units by sitting next to Bob."
+   "David would gain 41 happiness units by sitting next to Carol."])
+
+(deftest part-1-examples
+  (is (= 330 (sut/part-1 test-input))))
+
+(deftest challenge
+  (is (= 618 (sut/part-1 sut/input))
+      (= 601 (sut/part-2 sut/input))))
