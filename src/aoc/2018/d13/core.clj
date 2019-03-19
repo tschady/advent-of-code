e1a5df3fb769ac89b5682698a3f8d483026da7c2
tschady <tschady@users.noreply.github.com>
2019-03-19 16:48:15 -0500

    temp

--- src/aoc/2018/d13/core.clj
+++ src/aoc/2018/d13/core.clj
@@ -4,40 +4,34 @@
 (def input (file-util/read-lines "2018/d13.txt"))
 
 
-(def cart-map {\> {:track \-
-                   :dir :e
-                   :move [1 0]}
-               \< {:track \-
-                   :dir :w
-                   :move [-1 0]}
-               \^ {:track \|
-                   :dir :n
-                   :move [0 -1]}
-               \v {:track \|
-                   :dir :s
-                   :move [0 1]}})
+(def cart-map {\> {:underlying-track \-
+                   :move [1 0]
+                   :corner->turn {\/ \^,
+                                  \\ \v}}
+               \< {:underlying-track \-
+                   :move [-1 0]
+                   :corner->turn {\/ \v,
+                                  \\ \^}}
+               \^ {:underlying-track \|
+                   :move [0 -1]
+                   :corner->turn {\/ \>,
+                                  \\ \<}}
+               \v {:underlying-track \|
+                   :move [0 1]
+                   :corner->turn {\/ \<,
+                                  \\ \>}}})
 
 (defn cart? [c] (contains? cart-map c))
-
-(def ti ["/->-\\"
-         "|   |  /----\\"
-         "| /-+--+-\\  |"
-         "| | |  | v  |"
-         "\\-+-/  \\-+--/"
-         "\\------/"]
-)
-
 
 (defn- make-cart
   ""
   [[x y] c]
-  (let [dir (get-in cart-map [c :dir])]
-    (sorted-map [x y] {:dir dir :turns (cycle [:left :straight :right])})))
+  (sorted-map [x y] {:dir c :turns (cycle [:left :straight :right])}))
 
-(defn- build-world
+(defn- make-world
   "Return a world structure, which is a map of three elements:
-  :carts
   :track
+  :carts
   :crashes
   "
   [input]
@@ -48,16 +42,36 @@
                                         (make-cart [x y] c)
                                         {})
                                  track (if (not-empty cart)
-                                         (get-in cart-map [c :track])
+                                         (get-in cart-map [c :underlying-track])
                                          c)]
                              {:track (sorted-map [x y] track)
-                              :cart cart}))))
+                              :carts cart
+                              :crashes []}))))
 
-(build-world ti)
+
+(defn update-world
+  "Tick time once in world.  Carts move, turn, and sometimes crash.
+  Return new world object one time-step later."
+  [world]
+  (reduce (fn [acc cart]
+            (let )
+            )
+          world
+          (:carts world)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def ti ["/->-\\"
+         "|   |  /----\\"
+         "| /-+--+-\\  |"
+         "| | |  | v  |"
+         "\\-+-/  \\-+--/"
+         "\\------/"])
+
+(make-world ti)
 
 (def d (make-cart [0 0] \>))
 d
 
-(get-in d [[0 0] :turns])
-
-(update-in d [[0 0] :turns] #(drop 1 %))
+(first (get-in d [[0 0] :turns]))
+(update-in d [[0 0] :turns] rest)
