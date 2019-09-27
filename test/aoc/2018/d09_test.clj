aac98f8925477f19598a06d659311922e054250a
Tom Schady <tschady@gmail.com>
2019-09-27 10:12:20 -0500

    Create slow test target

--- test/aoc/2018/d09_test.clj
+++ test/aoc/2018/d09_test.clj
@@ -2,7 +2,7 @@
   (:require [aoc.2018.d09 :as sut]
             [clojure.test :refer :all]))
 
-(deftest part1-examples
+(deftest ^:slow part1-examples
   (are [output input] (= output (sut/part-1 input))
     32     "9 players; last marble is worth 25 points"
     8317   "10 players; last marble is worth 1618 points"
@@ -11,6 +11,6 @@
     54718  "21 players; last marble is worth 6111 points"
     37305  "30 players; last marble is worth 5807 points"))
 
-(deftest challenge
+(deftest ^:slow challenge
   (is (= 388131 (sut/part-1 sut/input)))
   (is (= 3239376988 (sut/part-2 sut/input))))
