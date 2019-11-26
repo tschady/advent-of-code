30f213b678c738a6bd932d800a0632102fdd88d4
tschady <tschady@users.noreply.github.com>
2019-11-26 05:25:54 -0600

    Remove redundant functions.  These are in string-util

--- src/aoc/file_util.clj
+++ src/aoc/file_util.clj
@@ -22,14 +22,3 @@
   "Return a list of rows, with each row as a list of values from the TSV."
   [path]
   (->> path read-lines (map #(str/split %1 #"\t"))))
-
-(defn explode-digits
-  "Turn input string of digits into sequence of numbers they represent."
-  [s]
-  (map #(Character/getNumericValue %) s))
-
-(def alphagram (comp sort str/lower-case))
-
-(defn anagram? [word candidate]
-  (and (not= (str/lower-case word) (str/lower-case candidate))
-       (= (alphagram word) (alphagram candidate))))
