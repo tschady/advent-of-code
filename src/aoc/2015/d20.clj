1343825bf57f7b8b91aa4f6f1308179d96a386d9
tschady <tschady@users.noreply.github.com>
2020-09-07 07:55:15 -0500

    Solve 2015 d20

---
+++ src/aoc/2015/d20.clj
@@ -0,0 +1,27 @@
+(ns aoc.2015.d20
+  (:require [aoc.math-util :as math-util]
+            [aoc.file-util :as file-util]))
+
+(def input (first (file-util/read-values "2015/d20.txt")))
+
+(defn- score [mult elves]
+  (reduce + 0 (map #(* mult %) elves)))
+
+(defn- elves
+  "Return set of elf numbers that visit `house`.  Elves visit each
+  house evenly divisible by itself, up to `limit` deliveries."
+  [limit house]
+  (remove #(> house (* limit %)) (math-util/factors house)))
+
+(defn- solve
+  "Returns lowest house number of house to get at least `present-target`."
+  [present-target score-mult elf-limit]
+  (->> (range)
+       (map (partial elves elf-limit))
+       (map (partial score score-mult))
+       (take-while (partial > present-target))
+       count))
+
+(defn part-1 [input] (solve input 10 Integer/MAX_VALUE))
+
+(defn part-2 [input] (solve input 11 50))
