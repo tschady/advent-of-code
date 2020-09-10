12150e4ed9911838353db3f9d5af00c04b46a966
tschady <tschady@users.noreply.github.com>
2020-09-10 06:45:32 -0500

    Solve 2015 d24

---
+++ src/aoc/2015/d24.clj
@@ -0,0 +1,23 @@
+(ns aoc.2015.d24
+  (:require [aoc.file-util :as file-util]
+            [clojure.math.combinatorics :as combo]))
+
+(def input (file-util/read-values "2015/d24.txt"))
+
+(defn qe-score [pkgs] (reduce * 1 pkgs))
+
+(defn find-target-pkg-group-score [pkgs groups]
+  (let [target-weight (/ (reduce + pkgs) groups)
+        max-group-size (Math/floor (/ (count pkgs) groups))]
+    (reduce (fn [res n]
+              (if (seq res)
+                (reduced (apply min (map qe-score res)))
+                (->> (combo/combinations pkgs n)
+                     (filter #(= target-weight (reduce + %)))
+                     (into res))))
+            '()
+            (range 1 max-group-size))))
+
+(defn part-1 [input] (find-target-pkg-group-score input 3))
+
+(defn part-2 [input] (find-target-pkg-group-score input 4))
