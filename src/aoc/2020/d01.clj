2fdedaed11e2252f701d2ce552514b475eac753f
tschady <tschady@users.noreply.github.com>
2020-12-01 04:09:22 -0600

    Solve 2020 d01, using slow library

---
+++ src/aoc/2020/d01.clj
@@ -0,0 +1,16 @@
+(ns aoc.2020.d01
+  (:require [aoc.file-util :as file-util]
+            [clojure.math.combinatorics :as combo]))
+
+(def input (file-util/read-values "2020/d01.txt"))
+
+(defn solve
+  "Returns the product of the `n` numbers from set of `input` that sum to `target-sum`"
+  [input target-sum n]
+  (->> (combo/combinations input n)
+       (filter #(= target-sum (apply + %)))
+       (apply reduce *)))
+
+(defn part-1 [input] (solve input 2020 2))
+
+(defn part-2 [input] (solve input 2020 3))
