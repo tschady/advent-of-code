44cfcbc0e6a02e2927c84af95d9406b6e7e1a6ec
tschady <tschady@users.noreply.github.com>
2020-12-05 06:38:24 -0600

    Simplify code conversion with string library

--- src/aoc/2020/d05.clj
+++ src/aoc/2020/d05.clj
@@ -1,15 +1,17 @@
 (ns aoc.2020.d05
-  (:require [aoc.file-util :as file-util]))
+  (:require [aoc.file-util :as file-util]
+            [clojure.string :as str]))
 
 (def input (file-util/read-lines "2020/d05.txt"))
 
-(defn seat-id [seat-map]
-  (let [binary-str (apply str (map {\B 1, \F 0, \R 1, \L 0} seat-map))]
-    (Integer/parseInt binary-str 2)))
+(defn seat-id [seat-code]
+  (-> seat-code
+      (str/escape {\B 1, \F 0, \R 1, \L 0})
+      (Integer/parseInt 2)))
 
 (defn part-1 [input] (apply max (map seat-id input)))
 
 (defn part-2 [input]
-  (let [full-seats (sort (map seat-id input))
-        all-seats (range (first full-seats) (inc (last full-seats)))]
-    (first (clojure.set/difference (set all-seats) (set full-seats)))))
+  (let [taken-seats (sort (map seat-id input))
+        all-seats (range (first taken-seats) (inc (last taken-seats)))]
+    (first (clojure.set/difference (set all-seats) (set taken-seats)))))
