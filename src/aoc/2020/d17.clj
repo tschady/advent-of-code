2e53c060b733d9e8df4f4d52a4096808582382d5
tschady <tschady@users.noreply.github.com>
2021-01-03 10:41:10 -0600

    Fix #15: Extract Conway's Game of Life function.

--- src/aoc/2020/d17.clj
+++ src/aoc/2020/d17.clj
@@ -1,33 +1,19 @@
 (ns aoc.2020.d17
-  (:require [aoc.file-util :as file-util]
+  (:require [aoc.conway-life :as life]
+            [aoc.file-util :as file-util]
             [aoc.grid :as grid]
-            [medley.core :as medley :refer [remove-vals filter-vals]]
-            [clojure.set :as set]))
+            [medley.core :as medley :refer [remove-vals]]))
 
 (def input (file-util/read-lines "2020/d17.txt"))
 
-(defn initial-active [dim input]
+(defn seed [dim input]
   (->> (grid/build-grid input {\. nil, \# :active})
        (remove-vals nil?)
        keys
        (map #(apply conj % (repeat (- dim 2) 0)))))
 
-(defn step-world [neighbor-fn actives]
-  (let [half
-        (reduce (fn [acc active-loc]
-                  (let [neighbors          (set (neighbor-fn active-loc))
-                        active-neighbors   (set/intersection neighbors (set actives))
-                        inactive-neighbors (set/difference neighbors active-neighbors)]
-                    (-> (if (<= 2 (count active-neighbors) 3)
-                          (update-in acc [:active] conj active-loc)
-                          acc)
-                        (update-in [:inactive] #(merge-with + % (frequencies inactive-neighbors))))))
-                {}
-                actives)]
-    (concat (:active half) (keys (filter-vals #(= 3 %) (:inactive half))))))
-
 (defn solve [input dim neighbor-fn]
-  (-> (iterate (partial step-world neighbor-fn) (initial-active dim input))
+  (-> (iterate (partial life/next-gen neighbor-fn #(<= 2 % 3) #(= 3 %)) (seed dim input))
       (nth 6)
       count))
 
