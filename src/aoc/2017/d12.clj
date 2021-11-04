0b0a75cf332da37dfb918c7f182a1fbde78828f9
tschady <tschady@users.noreply.github.com>
2021-11-03 20:37:59 -0500

    Solve 2017 day 12

---
+++ src/aoc/2017/d12.clj
@@ -0,0 +1,18 @@
+(ns aoc.2017.d12
+  (:require [aoc.file-util :as file-util]
+            [clojure.string :as str]
+            [ubergraph.core :as uber]
+            [ubergraph.alg :as alg]))
+
+(def input (file-util/read-lines "2017/d12.txt"))
+
+(defn parse-line [s] (read-string (str "{" (str/replace s #"<->" "[") "]}")))
+
+(defn make-graph [lines] (uber/graph (apply merge (map parse-line input))))
+
+(defn find-groups [graph] (alg/connected-components graph))
+
+(defn part-1 [input]
+  (count (set (first (filter #(some #{0} %) (find-groups (make-graph input)))))))
+
+(defn part-2 [input] (count (find-groups (make-graph input))))
