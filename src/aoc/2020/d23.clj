2ea2066e30a81c6b32f6f2cfee9c0a8743b0ae95
tschady <tschady@users.noreply.github.com>
2021-11-07 10:17:24 -0600

    Refactor 2020 day 23 into mutable int-array for 10x speed

--- src/aoc/2020/d23.clj
+++ src/aoc/2020/d23.clj
@@ -1,84 +1,52 @@
 (ns aoc.2020.d23
-  (:require [aoc.string-util :as string-util]))
+  (:require [aoc.string-util :as string-util]
+            [aoc.file-util :as file-util]))
 
-(def input (vec (string-util/explode-digits "186524973")))
+(def input (vec (string-util/explode-digits (file-util/read-file "2020/d23.txt"))))
 
-(defrecord Cup [label next])
-
-(defn make-cup [label] (->Cup label (ref nil)))
-
-(defn find-dest-label
-  [n max-n snippet-vals]
+(defn find-dest
+  [n max-n snippets]
   (let [target (dec n)]
     (cond
-      (contains? (set snippet-vals) target) (recur target max-n snippet-vals)
-      (>= 0 target) (recur (inc max-n) max-n snippet-vals)
+      (contains? snippets target) (recur target max-n snippets)
+      (>= 0 target) (recur max-n max-n snippets)
       :else target)))
 
-(defn get-next-labels
-  "Get the values of the next `n` nodes to the right of `cup`"
-  [cup n]
-  (->> cup
-       (iterate #(deref (:next %)))
+(defn make-ring [input size]
+  (let [ring (int-array (inc size))]
+    (->> [[0], input, (range 10 (inc size)), (vector (first input))]
+         (into [] cat)
+         (partition 2 1)
+         (map #(aset-int ring (first %) (second %)))
+         dorun)
+    ring))
+
+(defn play [input size turns]
+  (let [^ints ring (make-ring input size)]
+    (dotimes [_ turns]
+      (let [curr  (aget ring 0)
+            snip1 (aget ring curr)
+            snip2 (aget ring snip1)
+            snip3 (aget ring snip2)
+            curr2 (aget ring snip3)
+            dest  (find-dest curr (inc size) #{snip1 snip2 snip3})
+            dest2 (aget ring dest)]
+        (aset ring curr curr2)
+        (aset ring dest snip1)
+        (aset ring snip3 dest2)
+        (aset ring 0 curr2)))
+    ring))
+
+(defn get-nexts
+  "Return a collection of the next `n` nodes after `start`"
+  [ring start n]
+  (->> start
+       (iterate #(aget ^ints ring %))
        (drop 1)
-       (take n)
-       (map :label)))
-
-(defn init-state
-  "Construct the initial game state from ordered vector of input `labels`.
-  State is map of value (label) to node (cup), where each node is a value and mutable ref
-  to the next node in the chain.  State includes the current node (:cursor), and a cached
-  value of the maximum label.
-
-  The linked list lets us quickly change the order of the cups, and the hashmap lets us
-  jump to a node with a given value in constant time."
-  [labels max-label]
-  (let [ring (reduce (fn [state label]
-                       (let [new-cup (->Cup label (ref (get state :head)))]
-                         (-> state
-                             (update :label->cup conj [label new-cup])
-                             (assoc :head new-cup))))
-                     {:label->cup {}
-                      :head     nil}
-                     (reverse labels))
-        tail (get-in ring [:label->cup (last labels)])
-        head (get ring :head)]
-    (dosync (ref-set (:next tail) head))
-    (-> ring
-        (assoc :cursor (:head ring))
-        (assoc :max max-label))))
-
-(defn advance
-  "Return the node on the list `n` links forward."
-  [curr n]
-  (->> curr
-    (iterate #(deref (:next %)))
-    (take (inc n))
-    last))
-
-(defn move
-  "Perform one turn of the game, returning updated `state`."
-  [state]
-  (let [curr (:cursor state)
-        snippet-vals (get-next-labels curr 3)
-        snippet-head (get-in state [:label->cup (first snippet-vals)])
-        snippet-tail (get-in state [:label->cup (last snippet-vals)])
-        dest-label (find-dest-label (:label curr) (:max state) snippet-vals)
-        dest-cup (get-in state [:label->cup dest-label])]
-    (dosync
-     (ref-set (:next curr) (advance curr 4))
-     (ref-set (:next snippet-tail) (advance dest-cup 1))
-     (ref-set (:next dest-cup) snippet-head))
-    (assoc state :cursor (deref (:next curr)))))
+       (take n)))
 
 (defn part-1 [input]
-  (apply str (-> (iterate move (init-state input 9))
-                 (nth 100)
-                 (get-in [:label->cup 1])
-                 (get-next-labels 8))))
+  (apply str (get-nexts (play input 9 100) 1 8)))
 
 (defn part-2 [input]
-  (apply * (-> (iterate move (init-state (into input (range 10 1000001)) 1000000))
-               (nth 10000000)
-               (get-in [:label->cup 1])
-               (get-next-labels 2))))
+  (reduce * (get-nexts (play input 1e6 1e7) 1 2)))
