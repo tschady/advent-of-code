857535850be850b27f0a3de2d01f9d027cdb442e
tschady <tschady@users.noreply.github.com>
2021-11-14 12:45:32 -0600

    Add badge updater scripts and tooling

---
+++ script/update_badges.clj
@@ -0,0 +1,44 @@
+(ns update-badges
+  (:require [babashka.curl :as curl]
+            [babashka.pods :as pods]
+            [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(pods/load-pod 'retrogradeorbit/bootleg "0.1.9")
+
+(require '[pod.retrogradeorbit.bootleg.utils :refer [convert-to]]
+         '[pod.retrogradeorbit.hickory.select :as s])
+
+(def yrs ["2020" "2019" "2018" "2017" "2016" "2015"])
+
+(def cookie (slurp ".session"))
+
+(def badge-style
+  {"color"      "00cc00" ; right side
+   "labelColor" "0a0e25" ; left side
+   "style"      "for-the-badge"
+   "logo"       (str "data:image/png;base64," (slurp "img/aoc-favicon-base64"))})
+
+(defn get-stars
+  "Return a string representing number of stars earned for a given `year`"
+  [year]
+  (let [parsed (-> (str "adventofcode.com/" year)
+                   (curl/get {:headers {"Cookie" (str "session=" cookie)}})
+                   :body
+                   (convert-to :hickory))]
+    (-> (s/select (s/class "star-count") parsed)
+        first
+        :content
+        first)))
+
+(defn make-badge [year stars]
+  (let [params (merge {"label" year, "message" stars} badge-style)]
+    (:body (curl/get "http://img.shields.io/static/v1" {:query-params params}))))
+
+(defn save-badge! [year]
+  (let [path  (str "img/" year ".svg")
+        stars (get-stars year)
+        badge (make-badge year stars)]
+    (spit path badge)))
+
+(map save-badge! yrs)
