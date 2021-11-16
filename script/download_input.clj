63db07edf0c60f2a575f9f4a1852d3b06dbb82f9
tschady <tschady@users.noreply.github.com>
2021-11-16 17:20:40 -0600

    Add task to download problem input

---
+++ script/download_input.clj
@@ -0,0 +1,13 @@
+(ns download-input
+  (:require [babashka.curl :as curl]
+            [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(def cookie (slurp ".session"))
+
+(let [[yr dy] (read-string (first *command-line-args*))
+      url     (str "http://adventofcode.com/" yr "/day/" dy "/input")
+      dest    (format "resources/%s/d%02d.txt" yr (Long/parseLong dy))]
+  (->> (curl/get url {:headers {"Cookie" (str "session=" cookie)}})
+       :body
+       (spit dest)))
