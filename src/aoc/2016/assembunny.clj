3d33ad03780a892c7b535ba5936586f2bad2e854
tschady <tschady@users.noreply.github.com>
2022-11-27 06:50:40 -0600

    Extract assembunny interpreter into own module for later problems

---
+++ src/aoc/2016/assembunny.clj
@@ -0,0 +1,27 @@
+(ns aoc.2016.assembunny
+  (:require [clojure.string :as str]))
+
+(def init-state {:ptr 0 "a" 0 "b" 0 "c" 0 "d" 0})
+
+(defn- val-or-int [mem a] (or (get mem a) (Integer/parseInt a)))
+
+(defn- *cmd->fn [cmd]
+  (let [[op a & b] (str/split cmd #" ")]
+    (case op
+      "cpy" (fn [mem] (assoc! mem (first b) (val-or-int mem a)))
+      "inc" (fn [mem] (assoc! mem a (inc (get mem a))))
+      "dec" (fn [mem] (assoc! mem a (dec (get mem a))))
+      "jnz" (fn [mem] (if (not= 0 (val-or-int mem a))
+                             (assoc! mem :ptr (+ (get mem :ptr) -1 (Integer/parseInt (first b))))
+                             mem))
+      (throw (AssertionError. (str "Illegal input: " cmd))))))
+
+(def ^:private cmd->fn (memoize *cmd->fn))
+
+(defn run [prog memory]
+  (loop [mem (transient memory)]
+    (if-let [cmd (get prog (get mem :ptr))]
+      (recur (-> mem
+                 ((cmd->fn cmd))
+                 (assoc! :ptr (inc (get mem :ptr)))))
+      (get (persistent! mem) "a"))))
--- src/aoc/2016/d12.clj
+++ src/aoc/2016/d12.clj
@@ -1,34 +1,10 @@
 (ns aoc.2016.d12
-  (:require [aoc.file-util :as file-util]
+  (:require [aoc.2016.assembunny :as bunny]
+            [aoc.file-util :as file-util]
             [clojure.string :as str]))
 
 (def input (file-util/read-lines "2016/d12.txt"))
 
-(defn *cmd->fn [cmd]
-  (let [[op a & b] (str/split cmd #" ")]
-    (case op
-      "cpy" (fn [mem] (assoc! mem (first b) (or (get mem a) (Integer/parseInt a))))
-      "inc" (fn [mem] (assoc! mem a (inc (get mem a))))
-      "dec" (fn [mem] (assoc! mem a (dec (get mem a))))
-      "jnz" (fn [mem] (if (not= 0 (or (get mem a) (Integer/parseInt a)))
-                             (assoc! mem :ptr (+ (get mem :ptr) -1 (Integer/parseInt (first b))))
-                             mem))
-      (throw (AssertionError. (str "Illegal input: " cmd))))))
+(defn part-1 [input] (bunny/run input bunny/init-state))
 
-(def cmd->fn (memoize *cmd->fn))
-
-(defn run [prog memory]
-  (loop [mem (transient memory)]
-    (if-let [cmd (get prog (get mem :ptr))]
-      (recur (-> mem
-                 ((cmd->fn cmd))
-                 (assoc! :ptr (inc (get mem :ptr)))))
-      (persistent! mem))))
-
-(def init-state {:ptr 0 "a" 0 "b" 0 "c" 0 "d" 0})
-
-(defn part-1 [input] (-> input (run init-state) (get "a")))
-
-(defn part-2 [input] (-> input
-                         (run (assoc init-state "c" 1))
-                         (get "a")))
+(defn part-2 [input] (bunny/run input (assoc bunny/init-state "c" 1)))
