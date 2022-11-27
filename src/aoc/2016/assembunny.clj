17fff16b57c1631f0dcd4f45ca6b7c1e36ed0ae1
tschady <tschady@users.noreply.github.com>
2022-11-27 09:56:46 -0600

    Solve 2016 day 23

--- src/aoc/2016/assembunny.clj
+++ src/aoc/2016/assembunny.clj
@@ -5,21 +5,39 @@
 
 (defn- val-or-int [mem a] (or (get mem a) (Integer/parseInt a)))
 
+(defn- toggle-instr [s]
+  (when s
+    (case (first (str/split s #" "))
+      "inc" (str/replace-first s "inc" "dec")
+      "dec" (str/replace-first s "dec" "inc")
+      "tgl" (str/replace-first s "tgl" "inc")
+      "cpy" (str/replace-first s "cpy" "jnz")
+      "jnz" (str/replace-first s "jnz" "cpy")
+      s)))
+
+(defn- next-ptr [mem a] (+ (get mem :ptr) (val-or-int mem a)))
+
 (defn- *cmd->fn [cmd]
   (let [[op a & b] (str/split cmd #" ")]
-    (case op
-      "cpy" (fn [mem] (assoc! mem (first b) (val-or-int mem a)))
-      "inc" (fn [mem] (assoc! mem a (inc (get mem a))))
-      "dec" (fn [mem] (assoc! mem a (dec (get mem a))))
-      "jnz" (fn [mem] (if (not= 0 (val-or-int mem a))
-                             (assoc! mem :ptr (+ (get mem :ptr) -1 (Integer/parseInt (first b))))
-                             mem))
-      (throw (AssertionError. (str "Illegal input: " cmd))))))
+    (try
+      (case op
+        "cpy" (fn [mem] (assoc! mem (first b) (val-or-int mem a)))
+        "inc" (fn [mem] (assoc! mem a (inc (get mem a))))
+        "dec" (fn [mem] (assoc! mem a (dec (get mem a))))
+        "jnz" (fn [mem] (if (not= 0 (val-or-int mem a))
+                          (assoc! mem :ptr (dec (next-ptr mem (first b))))
+                          mem))
+        "tgl" (fn [mem]
+                (let [target (next-ptr mem a)]
+                  (if-let [instr (get-in mem [:prog target])]
+                    (assoc! mem :prog (assoc (get mem :prog) target (toggle-instr instr)))
+                    mem))))
+      (catch Exception e (identity)))))
 
 (def ^:private cmd->fn (memoize *cmd->fn))
 
 (defn run [prog memory]
-  (loop [mem (assoc! (transient memory) :prog prog)]
+  (loop [mem (transient (assoc memory :prog prog))]
     (if-let [cmd (get-in mem [:prog (get mem :ptr)])]
       (recur (-> mem
                  ((cmd->fn cmd))
