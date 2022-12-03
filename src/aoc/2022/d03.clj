fdf3f739e65e8c4a06b902183f8a0781effd265d
tschady <tschady@users.noreply.github.com>
2022-12-03 05:02:50 -0600

    Solve 2022 day 3

---
+++ src/aoc/2022/d03.clj
@@ -0,0 +1,24 @@
+(ns aoc.2022.d03
+  (:require
+   [aoc.file-util :as f]
+   [aoc.string-util :as s]
+   [clojure.set :as set]))
+
+(def input (f/read-lines "2022/d03.txt"))
+
+(def priority (zipmap (str s/alphabet-lower s/alphabet-upper) (range 1 53)))
+
+(defn common-item [sacks]
+  (first (apply set/intersection (map set sacks))))
+
+(defn solve [data split-fn]
+  (->> (split-fn data)
+       (map common-item)
+       (map priority)
+       (reduce +)))
+
+(defn part-1 [input]
+  (solve input (fn [sacks] (map #(partition (/ (count %) 2) %) sacks))))
+
+(defn part-2 [input]
+  (solve input (partial partition 3)))
