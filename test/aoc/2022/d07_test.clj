666021a4c0ea421b3e153b7588b450cbf7110d41
tschady <tschady@users.noreply.github.com>
2022-12-07 08:33:32 -0600

    Solve 2022 day 7

---
+++ test/aoc/2022/d07_test.clj
@@ -0,0 +1,34 @@
+(ns aoc.2022.d07-test
+  (:require
+   [aoc.2022.d07 :as sut]
+   [clojure.test :refer :all]
+   [clojure.string :as str]))
+
+(def example (str/split-lines
+"$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k"))
+
+(deftest challenges
+  (is (= 1582412 (sut/part-1 sut/input)))
+  (is (= 3696336 (sut/part-2 sut/input))))
