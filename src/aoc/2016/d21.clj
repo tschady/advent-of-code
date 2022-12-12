8e28515cd30fd4cc8512dd5374148afadaf6db22
tschady <tschady@users.noreply.github.com>
2022-12-11 20:06:10 -0600

    Extract common string rotation methods

--- src/aoc/2016/d21.clj
+++ src/aoc/2016/d21.clj
@@ -1,29 +1,17 @@
 (ns aoc.2016.d21
   (:require
    [aoc.file-util :as file-util]
-   [aoc.string-util :as string-util]
+   [aoc.string-util :as s]
    [clojure.string :as str]))
 
 (def input (file-util/read-lines "2016/d21.txt"))
-
-(defn swap-letter [s x y] (replace {x y, y x} s))
-
-(defn swap-pos [s i j] (swap-letter s (nth s i) (nth s j)))
-
-(defn rotate-left [s n]
-  (let [i (mod n (count s))]
-    (concat (drop i s) (take i s))))
-
-(defn rotate-right [s n]
-  (let [i (mod n (count s))]
-    (concat (take-last i s) (drop-last i s))))
 
 (defn rotate-based [s c]
   (let [i (.indexOf s c)
         n (if (>= i 4)
             (+ 2 i)
             (+ 1 i))]
-    (rotate-right s n)))
+    (s/rotate-right s n)))
 
 (defn move-pos [s i j]
   (let [letter (nth s i)
@@ -41,13 +29,13 @@
 
 (defn transform [s instr]
   (condp (fn [test subj] (str/starts-with? subj test)) instr
-    "swap position" (apply swap-pos s (string-util/ints instr))
-    "swap letter"   (apply swap-letter s (get-single-chars instr))
-    "rotate left"   (apply rotate-left s (string-util/ints instr))
-    "rotate right"  (apply rotate-right s (string-util/ints instr))
+    "swap position" (apply s/swap-pos s (s/ints instr))
+    "swap letter"   (apply s/swap-letter s (get-single-chars instr))
+    "rotate left"   (apply s/rotate-left s (s/ints instr))
+    "rotate right"  (apply s/rotate-right s (s/ints instr))
     "rotate based"  (apply rotate-based s (get-single-chars instr))
-    "reverse"       (apply reverse-pos s (string-util/ints instr))
-    "move"          (apply move-pos s (string-util/ints instr))))
+    "reverse"       (apply reverse-pos s (s/ints instr))
+    "move"          (apply move-pos s (s/ints instr))))
 
 ;; brute force this one until rolling forward is a match
 (defn inv-rotate-based
@@ -59,13 +47,13 @@
 
 (defn inv-transform [s instr]
   (condp (fn [test subj] (str/starts-with? subj test)) instr
-    "swap position" (apply swap-pos s (reverse (string-util/ints instr)))
-    "swap letter"   (apply swap-letter s (get-single-chars instr))
-    "rotate left"   (apply rotate-right s (string-util/ints instr))
-    "rotate right"  (apply rotate-left s (string-util/ints instr))
+    "swap position" (apply s/swap-pos s (reverse (s/ints instr)))
+    "swap letter"   (apply s/swap-letter s (get-single-chars instr))
+    "rotate left"   (apply s/rotate-right s (s/ints instr))
+    "rotate right"  (apply s/rotate-left s (s/ints instr))
     "rotate based"  (apply inv-rotate-based s (get-single-chars instr))
-    "reverse"       (apply reverse-pos s (string-util/ints instr))
-    "move"          (apply move-pos s (reverse (string-util/ints instr)))))
+    "reverse"       (apply reverse-pos s (s/ints instr))
+    "move"          (apply move-pos s (reverse (s/ints instr)))))
 
 (defn part-1 [s prog] (apply str (reduce transform (seq s) prog)))
 
